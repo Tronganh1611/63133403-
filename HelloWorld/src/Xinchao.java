23a90dc1264b67cf477cb0b2bc0ce5c52cb519aa
Tronganh1611 <tronganh16112003@gmail.com>
2024-01-19 11:48:36 +0700

    Cập nhật code
    Sửa mới và thêm

--- HelloWorld/src/Xinchao.java
+++ HelloWorld/src/Xinchao.java
@@ -13,6 +13,7 @@ public class Xinchao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("TÔI LẦN ĐẦU HỌC JAVA, đang thư nghiệm quản lí với mã nguồn GIT");
+		System.out.print("Tôi thêm một dòng mới và sửa lại chữ java ở trên");
 	}
 
 }
