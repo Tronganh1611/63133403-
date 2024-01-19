2b5c7285144300d1d7035b8cc7ee63f7622c1f44
Tronganh1611 <tronganh16112003@gmail.com>
2024-01-19 11:46:15 +0700

    Ví dụ đầu tiên
    Chỉ để demo

---
+++ HelloWorld/src/Xinchao.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Xinchao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TÔI LẦN ĐẦU HỌC JAVA, đang thư nghiệm quản lí với mã nguồn GIT");
+	}
+
+}
