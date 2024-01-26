f99e758de196d90041ad437e55832c60f7d92820
Tronganh1611 <tronganh16112003@gmail.com>
2024-01-26 13:32:56 +0700

    Lad1Bai1

---
+++ HelloWorld/src/SayHelloApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class SayHelloApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chào. Tôi là SV khoa CNTT-NTU: Học lâpj trình java");
+		System.out.print("Bùi TRọng Anh");
+
+	}
+
+}
