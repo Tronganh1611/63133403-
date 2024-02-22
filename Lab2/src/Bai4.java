6631454341a3752088fbae89523e98b814605602
Tronganh1611 <tronganh16112003@gmail.com>
2024-02-22 19:26:21 +0700

    Lab2Bai4
    Chưa xong

---
+++ Lab2/src/Bai4.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Bai4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int ma;
+            System.out.println("+---------------------------------------------------+");
+            System.out.println("1. Giải phương trình bậc nhất");
+            System.out.println("2. Giải phương trình bậc 2");
+            System.out.println("3. Tính tiền điện");
+            System.out.println("4. Kết thúc");
+            System.out.println("+---------------------------------------------------+");
+            System.out.print("Chọn chức năng: ");
+            ma = scanner.nextInt();
+
+
+}
