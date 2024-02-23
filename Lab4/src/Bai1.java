31f29a202e6bc956a3112c7bc65c10e7eec71ae2
Tronganh1611 <tronganh16112003@gmail.com>
2024-02-23 12:37:53 +0700

    Update Lab4Bai1
    làm xong bài 1

--- Lab4/src/Bai1.java
+++ Lab4/src/Bai1.java
@@ -1,13 +1,40 @@
+import java.util.Scanner;
 
 public class Bai1 {
+    private static String tenSp;
+    private static double donGia;
+    private static double giamGia;
 
-	public Bai1() {
-		// TODO Auto-generated constructor stub
-	}
+    public Bai1(String TenSp, double DonGia, double GiamGia ) {
+    	TenSp = tenSp;
+    	DonGia =donGia;
+    	GiamGia = giamGia;
+    	
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void nhap() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập tên sản phẩm: ");
+        tenSp = scanner.nextLine();
+        System.out.print("Nhập đơn giá: ");
+        donGia = scanner.nextDouble();
+        System.out.print("Nhập giảm giá: ");
+        giamGia = scanner.nextDouble();
+    }
 
+    public static double getThueNhapKhau() {
+        return donGia * 0.1;
+    }
+
+    public static void xuat() {
+        System.out.println("Tên sản phẩm: " + tenSp);
+        System.out.println("Đơn giá: " + donGia);
+        System.out.println("Giảm giá: " + giamGia);
+        System.out.println("Thuế nhập khẩu: " + getThueNhapKhau());
+    }
+    public static void main(String[] args) {
+		nhap();
+		xuat();
 	}
 
 }
