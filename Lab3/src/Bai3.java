c2afc19c4aa568cebc7882870cd52c0c32e74db0
Tronganh1611 <tronganh16112003@gmail.com>
2024-02-23 10:01:55 +0700

    Update Lab3Bai3
    đã xong bài 3

--- Lab3/src/Bai3.java
+++ Lab3/src/Bai3.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Bai3 {
@@ -10,7 +11,26 @@ public class Bai3 {
             System.out.print("Nhập phần tử thứ " + (i + 1) + ": ");
             a[i] = scanner.nextInt();
         }
-
-        
+        System.out.println();
+        Arrays.sort(a);
+        System.out.print("Mảng sau khi sắp xếp :");
+        for (int i = 0; i < n; i++){
+        	System.out.print(a[i]+" ");
+        }
+        System.out.println();
+        int min = a[0];
+        for (int i = 1; i < n; i++) min = Math.min(min, a[i]);
+        System.out.print("Phần tử nhỏ nhất của mảng là "+min);
+        System.out.println();
+        int d = 0, tong =0;
+        float tbc=0;
+        for (int i = 0; i < n; i++){
+        	if (a[i] % 3 == 0) {
+        		d++;
+        		tong=tong+a[i];
+        	}
+        }
+        tbc = tong/d;
+        System.out.printf("Trung bình cộng các phần tử chia hết cho 3 là %.2f",tbc);
     }
 }
