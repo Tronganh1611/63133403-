996a5841664b4363e434aa7fdc3d93a049d028ca
Tronganh1611 <tronganh16112003@gmail.com>
2024-03-01 11:39:15 +0700

    Update Lab4Bai3

--- Lab4/src/Bai3.java
+++ Lab4/src/Bai3.java
@@ -1,8 +1,66 @@
+import java.util.Scanner;
 
 public class Bai3 {
+    private static String tenSp;
+    private static double donGia;
+    private static double giamGia;
 
-	public Bai3() {
-		// TODO Auto-generated constructor stub
+    public Bai3(String TenSp, double DonGia, double GiamGia ) {
+    	TenSp = tenSp;
+    	DonGia =donGia;
+    	GiamGia = giamGia;
+    	
+    }
+    
+
+    public Bai3(String TenSp, double DonGia) {
+    	TenSp = tenSp;
+    	DonGia =donGia;
+    	
+    }
+
+	public static void nhapgia() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập tên sản phẩm: ");
+        tenSp = scanner.nextLine();
+        System.out.print("Nhập đơn giá: ");
+        donGia = scanner.nextDouble();
+        System.out.print("Nhập giảm giá: ");
+        giamGia = scanner.nextDouble();
+    }
+	public static void nhap() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập tên sản phẩm: ");
+        tenSp = scanner.nextLine();
+        System.out.print("Nhập đơn giá: ");
+        donGia = scanner.nextDouble();
+    }
+
+    private static  double getThueNhapKhau() {
+        return donGia * 0.1;
+    }
+
+    public static void xuatgia() {
+        System.out.println("\nTên sản phẩm: " + tenSp);
+        System.out.println("Đơn giá: " + donGia);
+        System.out.println("Giảm giá: " + giamGia);
+        System.out.println("Thuế nhập khẩu: " + getThueNhapKhau());
+        System.out.println();
+    }
+    public static void xuat() {
+        System.out.println("\nTên sản phẩm: " + tenSp);
+        System.out.println("Đơn giá: " + donGia);
+        System.out.println("Thuế nhập khẩu: " + getThueNhapKhau());
+        System.out.println();
+    }
+    public static void main(String[] args) {
+		Bai3 sp1 = new Bai3(tenSp,donGia);
+		Bai3 sp2 = new Bai3(tenSp, donGia, giamGia);
+		sp1.nhap();
+		sp1.xuat();
+		
+		sp2.nhapgia();
+		sp2.xuatgia();
 	}
 
 }
